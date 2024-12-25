e15683b282de748019dc42c9dbb4eed01272f649
FareZrextIT <faris.milic11@gmail.com>
2024-12-25 19:02:26 +0100

    promjenjeno na bos

--- hrana.js
+++ hrana.js
@@ -1,24 +1,24 @@
-import { onSnake, expandSnake } from './snake.js'
-import { randomGridPosition } from './grid.js'
+import { onZmija, expandZmija } from './zmija.js'
+import { randomMrezaPosition } from './mreza.js'
 let hrana = getRandomHranaPosition()
 const EXPANSION_RATE = 5
 export function update() {
-  if (onSnake(hrana)) {
-    expandSnake(EXPANSION_RATE)
+  if (onZmija(hrana)) {
+    expandZmija(EXPANSION_RATE)
     hrana = getRandomHranaPosition()
   }
 }
 export function draw(gameBoard) {
   const hranaElement = document.createElement('div')
-  hranaElement.style.gridRowStart = hrana.y
-  hranaElement.style.gridColumnStart = hrana.x
+  hranaElement.style.mrezaRowStart = hrana.y
+  hranaElement.style.mrezaColumnStart = hrana.x
   hranaElement.classList.add('hrana')
   gameBoard.appendChild(hranaElement)
 }
 function getRandomHranaPosition() {
   let newHranaPosition
-  while (newHranaPosition == null || onSnake(newHranaPosition)) {
-    newHranaPosition = randomGridPosition()
+  while (newHranaPosition == null || onZmija(newHranaPosition)) {
+    newHranaPosition = randomMrezaPosition()
   }
   return newHranaPosition
 }
