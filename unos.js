a46f541708968bf724757d21ab83bdbc3a8ccf8e
FareZrextIT <faris.milic11@gmail.com>
2024-12-25 19:44:24 +0100

    Izmijenjeno

--- unos.js
+++ unos.js
@@ -1,28 +1,28 @@
-let unosDirection = { x: 0, y: 0 }
-let lastUnosDirection = { x: 0, y: 0 }
+let inputDirection = { x: 0, y: 0 }
+let lastInputDirection = { x: 0, y: 0 }
 
 window.addEventListener('keydown', e => {
   switch (e.key) {
     case 'ArrowUp':
-      if (lastUnosDirection.y !== 0) break
-      unosDirection = { x: 0, y: -1 }
+      if (lastInputDirection.y !== 0) break
+      inputDirection = { x: 0, y: -1 }
       break
     case 'ArrowDown':
-      if (lastUnosDirection.y !== 0) break
-      unosDirection = { x: 0, y: 1 }
+      if (lastInputDirection.y !== 0) break
+      inputDirection = { x: 0, y: 1 }
       break
     case 'ArrowLeft':
-      if (lastUnoDirection.x !== 0) break
-      unosDirection = { x: -1, y: 0 }
+      if (lastInputDirection.x !== 0) break
+      inputDirection = { x: -1, y: 0 }
       break
     case 'ArrowRight':
-      if (lastUnosDirection.x !== 0) break
-      unosDirection = { x: 1, y: 0 }
+      if (lastInputDirection.x !== 0) break
+      inputDirection = { x: 1, y: 0 }
       break
   }
 })
 
-export function getUnosDirection() {
-  lastUnosDirection = unosDirection
-  return unosDirection
+export function getInputDirection() {
+  lastInputDirection = inputDirection
+  return inputDirection
 }
